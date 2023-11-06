1580c0f4b7ad2d8f49e823e8b6e2e96309e72b31
Shahbaz Yousaf <shahbazyousaff@gmail.com>
2023-11-06 22:29:12 +0000

    made changes to the monitoring agent scripts

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -18,17 +18,19 @@ memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
 cpu_idle=$(echo "$vmstat_mb" | awk '{print $15}' | tail -n1 | xargs)
 cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -n1 | xargs)
 disk_io=$(vmstat -d | awk '{print $10}' | tail -n1 | xargs)
-disk_available=$(df -BM / | awk '{print $10}')
+#disk_available=$(df -BM / | awk '{print $4}')
+disk_available=$(df -BM / | awk 'NR == 2 {print $4}' | grep -o '[0-9]\+' | xargs)
+
 
 
 timestamp=$(vmstat -t | awk '{print $18, $19}' | tail -n1)
 
 
 
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+#host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
 
 
-insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES('$timestamp', '$host_id', '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available')";
+insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES('$timestamp', (SELECT id FROM host_info WHERE hostname='$hostname'), '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available')";
 
 #set up env var for pql cmd
 export PGPASSWORD=$psql_password
